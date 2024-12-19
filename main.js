04c9442b5d82c563bab0dce33f9cd2005af509fd
sushanthj674 <153906968+sushanthj674@users.noreply.github.com>
2024-12-19 15:33:09 +0530

    added change dir function

--- main.js
+++ main.js
@@ -1,15 +1,31 @@
 let isWantToExe = true;
-let paths = ['cashew ~ '];
+const home = 'cashew ~ ';
+let paths = [];
 
-while (isWantToExe) {
-  const [command, value] = prompt(paths.join('') + ' %').split(' ');
+const rmDots = function (value) {
+  return value !== '..';
+};
+
+const changeDir = function (path) {
+  if (path[0] + path[1] === '..') {
+    paths.pop();
+  }
+  paths = paths.concat(path.split('/').filter(rmDots));
+  return '';
+};
+
+const executeCommands = function (command, value) {
   switch (command) {
     case 'end':
       isWantToExe = false;
-      break;
-    case 'cd': paths = paths.concat(value.split('/'));
-      break;
-    default: console.log('error: unknown command " ' + command + ' "');
+      return '';
+    case 'cd':
+      return changeDir(value);
+    default: return ('error: unknown command " ' + command + ' "');
   }
+};
 
+while (isWantToExe) {
+  const [command, value] = prompt(home + paths.join('/') + ' %').split(' ');
+  console.log(executeCommands(command, value));
 }
